f7be29767056857004d1b9efa3cb85438e108179
Viren Jawadwar <virenjawadwar001@gmail.com>
2025-07-26 19:36:17 +0530

    updated the code by adding main

--- a143.java
+++ a143.java
@@ -1,57 +1,71 @@
 import java.util.*;
-// max sum subarray kadane's algo
-public class a143 {
-    public static void subSum(int numbers[]) {
-        int currSum=0;
-        int maxSum=Integer.MIN_VALUE;
-        int prefix[]=new int[numbers.length];
-        prefix[0]=numbers[0];
 
-        //calculate prefi<numbersay
-        for(int i=1;i<prefix.length;i++) {
-            prefix[i]=prefix[i-1]+numbers[i];
+// Max sum subarray using prefix sum and Kadane's algorithm
+public class a143 {
+
+    // Brute-force with prefix sum
+    public static void subSum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+        prefix[0] = numbers[0];
+
+        // calculate prefix array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
         }
 
-        for(int i=0;i<numbers.length;i++) {
-            int start=i;
-            for(int j=i;j<numbers.length;j++) {
-                int end=j;
-
-                currSum=start==0?prefix[end]: prefix[end]-prefix[start-1];
-               
-                if(maxSum<currSum) {
-                    maxSum=currSum;
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (maxSum < currSum) {
+                    maxSum = currSum;
                 }
             }
         }
-        System.out.println("max sum:"+maxSum);
+        System.out.println("Max sum using prefix sum: " + maxSum);
     }
-      public static void kadane(int numbers[]) {
-    int ms = Integer.MIN_VALUE;
-    int cs = 0;
-    boolean allNegative = true;
-    int maxElement = numbers[0];
 
-    for (int i = 0; i < numbers.length; i++) {
-        cs = cs + numbers[i];
+    // Kadane's Algorithm
+    public static void kadane(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        boolean allNegative = true;
+        int maxElement = numbers[0];
 
-        if (numbers[i] >= 0) {
-            allNegative = false;
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+
+            if (numbers[i] >= 0) {
+                allNegative = false;
+            }
+
+            maxElement = Math.max(maxElement, numbers[i]);
+
+            if (cs < 0) {
+                cs = 0;
+            }
+
+            ms = Math.max(cs, ms);
         }
 
-        maxElement = Math.max(maxElement, numbers[i]);
-
-        if (cs < 0) {
-            cs = 0;
+        if (allNegative) {
+            System.out.println("All elements are negative. Max subarray sum is: " + maxElement);
+        } else {
+            System.out.println("Max subarray sum using Kadane's Algorithm: " + ms);
         }
-
-        ms = Math.max(cs, ms);
     }
 
-    if (allNegative) {
-        System.out.println("All elements are negative. Max subarray sum is: " + maxElement);
-    } else {
-        System.out.println("Max subarray sum is = " + ms);
+    // Main method
+    public static void main(String[] args) {
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+        System.out.println("Using Prefix Sum:");
+        subSum(numbers);
+
+        System.out.println("Using Kadane's Algorithm:");
+        kadane(numbers);
     }
-}
 }
