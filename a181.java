66214ef6a0aa933c2b0cdee98e171a5e53682452
Viren Jawadwar <virenjawadwar001@gmail.com>
2025-08-25 21:39:45 +0530

    odd or even with bit logic

---
+++ a181.java
@@ -0,0 +1,18 @@
+public class a181 {
+    //check if odd or even
+
+    public static void oddOrEven(int n) {
+        int bitmask=1;
+        if((n&bitmask)==0) {
+            //even number 
+            System.out.println("even number");
+        } else {
+            System.out.println("odd number");
+        }
+    }
+
+    public static void main(String args[]) {
+        oddOrEven(3);
+        oddOrEven(12);
+    }
+}
